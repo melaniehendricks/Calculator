041c750d7aa9289c0d56dcda5d4293e961991638
melaniehendricks <melaniehendricks@192.168.0.6>
2019-02-23 12:20:16 -0700

    Initial commit of project

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,74 @@
+/* 
+ * Melanie Hendricks
+ * ID: 402
+ * Assignment 2
+ */
+package cse360assign2;
+
+/**
+ * Represents a calculator
+ * @author melaniehendricks
+ *
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	/**
+	 * gets total
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * addition function
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	
+	/**
+	 * subtraction function
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	
+	/**
+	 * multiplication function
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	
+	/**
+	 * division multiplication
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	
+	/**
+	 * gets history of all actions 
+	 * @return
+	 */
+	public String getHistory () {
+		return "";
+	}
+}

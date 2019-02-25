3101a858f6e465f3f656393361d26a8a2c20ede3
melaniehendricks <melaniehendricks@melanies-mbp.3520.dhcp.asu.edu>
2019-02-25 11:51:59 -0700

    added new variables, changed arithmetic and getHistory methods

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -6,69 +6,126 @@
 package cse360assign2;
 
 /**
- * Represents a calculator
+ * Represents a calculator that can add, subtract, multiply and divide two integers. 
  * @author melaniehendricks
- *
+ * 
  */
 
-public class Calculator {
+public class Calculator 
+{
 
 	private int total;
-	
-	public Calculator () {
-		total = 0;  // not needed - included for clarity
-	}
+	private String operation;
+	private String number;
 	
 	
 	/**
-	 * gets total
-	 * @return
+	 * Represents a constructor that sets initial total to 0 and
+	 * ensures that first number in the history is 0. 
 	 */
-	public int getTotal () {
-		return 0;
-	}
-	
-	/**
-	 * addition function
-	 * @param value
-	 */
-	public void add (int value) {
+	public Calculator () 
+	{
+		total = 0;  
+		number = Integer.toString(total);
+		operation = number + " "; 
+		getHistory();
 		
 	}
 	
 	
+	
 	/**
-	 * subtraction function
-	 * @param value
+	 * Gets total
+	 * @return total An integer that represents the total after an operation 
 	 */
-	public void subtract (int value) {
+	public int getTotal () 
+	{
+		return total;
+	}
+	
+	
+	
+	/**
+	 * Addition function
+	 * Converts integer to String for getHistory() method.
+	 * Adds value to total. 
+	 * @param value An integer to be used in an operation. 
+	 */
+	public void add (int value) 
+	{
+		number = Integer.toString(value);
+		operation = operation + "+ " + number + " ";
+		getHistory();
+		total = total + value;
+		getTotal();
 		
 	}
 	
 	
+	
 	/**
-	 * multiplication function
-	 * @param value
+	 * Subtraction function
+	 * Converts integer to String for getHistory() method.
+	 * Subtracts value from total. 
+	 * @param value An integer to be used in an operation. 
 	 */
-	public void multiply (int value) {
-		
+	public void subtract (int value) 
+	{
+		number = Integer.toString(value);
+		operation = operation + "- " + number + " ";
+		getHistory();
+		total = total - value;
+		getTotal();
 	}
 	
 	
+	
 	/**
-	 * division multiplication
-	 * @param value
+	 * Multiplication function
+	 * Converts integer to String for getHistory() method.
+	 * Multiplies total by value 
+	 * @param value An integer to be used in an operation. 
 	 */
-	public void divide (int value) {
-		
+	public void multiply (int value) 
+	{
+		number = Integer.toString(value);
+		operation = operation + "* " + number + " ";
+		getHistory();
+		total = total * value;
+		getTotal();
 	}
 	
 	
+	
 	/**
-	 * gets history of all actions 
-	 * @return
+	 * Division function 
+	 * Converts integer to String for getHistory() method.
+	 * Divides total by value. 
+	 * @param value An integer to be used in an operation. 
 	 */
-	public String getHistory () {
-		return "";
+	public void divide (int value) 
+	{
+		number = Integer.toString(value);
+		operation = operation + " / " + number + " ";
+		getHistory();
+		if (value == 0)
+		{
+			total = 0;
+		}
+		else
+			total = total / value;
+		getTotal();
+	}
+	
+	
+	
+	/**
+	 * Gets history of all actions. 
+	 * @return A String representing the history of all values and 
+	 * operations done within the program. 
+	 */
+	public String getHistory () 
+	{
+		return operation;	
 	}
 }

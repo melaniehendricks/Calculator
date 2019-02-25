9c4d35ce0d8ce9e6c22acc334413166db571dc8e
melaniehendricks <melaniehendricks@melanies-mbp.3520.dhcp.asu.edu>
2019-02-25 11:51:59 -0700

    completed add, subtract, multiply and divide methods

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -25,7 +25,7 @@ public class Calculator {
 	 * @return
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
@@ -33,7 +33,7 @@ public class Calculator {
 	 * @param value
 	 */
 	public void add (int value) {
-		
+		total = total + value;
 	}
 	
 	
@@ -42,7 +42,7 @@ public class Calculator {
 	 * @param value
 	 */
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
 	
 	
@@ -51,7 +51,7 @@ public class Calculator {
 	 * @param value
 	 */
 	public void multiply (int value) {
-		
+		total = total * value;
 	}
 	
 	
@@ -60,7 +60,11 @@ public class Calculator {
 	 * @param value
 	 */
 	public void divide (int value) {
-		
+		if (value == 0){
+			total = 0;
+		}
+		else
+			total = total / value;
 	}
 	
 	
